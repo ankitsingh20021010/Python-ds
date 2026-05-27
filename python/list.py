489fa0785fdb5ed97deeab3159e3416fb3bd44ad
Ankit Singh <137784986+ankitsingh20021010@users.noreply.github.com>
2026-05-27 10:32:52 +0530

    Create list.py to demonstrate list operations in Python
    Added a Python script demonstrating list usage, including indexing and mutability.

---
+++ python/list.py
@@ -0,0 +1,18 @@
+#list in python 
+#list is just like array 
+num=[1,2,3,4,5,6]
+print(num)
+print(type(num))
+#indexing in list
+print(num[0])
+print(num[1])
+print(num[-1])
+#-1 use for print last value if you don't last element index num
+#multiple data type support in single list such as int flot str
+list2=["name", "Ankit","age",23]
+print(list2)
+#list are mutable in python means num[1]=5 you change num[1]=10
+print(num[1],"<--num[1]=2")
+print("I want to change num [1]=2 change 5")
+num[1]=5
+print(num[1],"<--num[1]=5")
